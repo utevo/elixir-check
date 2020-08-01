b4214ee98d7ddddbf63c3b93f0846e945c493f8d
utevo <utevo@pop-os.localdomain>
2020-08-01 13:47:23 +0200

    feat: add deal, save, load

--- lib/cards.ex
+++ lib/cards.ex
@@ -3,7 +3,10 @@ defmodule Cards do
   Documentation for `Cards`.
   """
   def create do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values, do: "#{value} of #{suit}"
   end
 
   def shuffle(dec) do
@@ -12,5 +15,19 @@ defmodule Cards do
 
   def contains?(dec, card) do
     Enum.member?(dec, card)
+  end
+
+  def deal(dec, hand_size) do
+    Enum.split(dec, hand_size)
+  end
+
+  def save(dec, path) do
+    binary = :erlang.term_to_binary(dec)
+    File.write(path, binary)
+  end
+
+  def load(path) do
+    {:ok, binary} = File.read(path)
+    :erlang.binary_to_term(binary)
   end
 end
